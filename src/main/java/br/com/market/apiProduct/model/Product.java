76e79181781ec43b23c8ef43ebd0f9dfcc2b7737
Gabriel de Farias <Gabriellongarai.gl@gmail.com>
2025-06-23 10:06:24 -0300

    adicao de Bean validation

--- src/main/java/br/com/market/apiProduct/model/Product.java
+++ src/main/java/br/com/market/apiProduct/model/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 
 @Entity(name = "product")
 public class Product {
@@ -11,8 +13,11 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Size(min = 3, max = 50)
     private String name;
+    @Min(0)
     private double price;
+    @Min(0)
     private int quantity;
 
     public Product() {
